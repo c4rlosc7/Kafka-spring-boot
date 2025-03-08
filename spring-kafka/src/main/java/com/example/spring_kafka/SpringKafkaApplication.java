be3b1c15c411344f2b2548c9882eb779f9b38011
Carlos Martinez <camartinez7@hotmail.com>
2025-03-07 21:48:16 -0500

    spring project and kafka implement consumer

--- spring-kafka/src/main/java/com/example/spring_kafka/SpringKafkaApplication.java
+++ spring-kafka/src/main/java/com/example/spring_kafka/SpringKafkaApplication.java
@@ -1,10 +1,23 @@
 package com.example.spring_kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class SpringKafkaApplication {
+
+	public static final String TOPIC_TEST_00 = "topic-test-00";
+	public static final String CARLOS_GROUP_CONSUMER = "carlos-group-consumer";
+
+	private static final Logger log = LoggerFactory.getLogger(SpringKafkaApplication.class);
+
+	@KafkaListener(topics = TOPIC_TEST_00, groupId = CARLOS_GROUP_CONSUMER)
+	public void listen(String message) {
+		log.info("Message received {} ", message);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringKafkaApplication.class, args);
---
+++ spring-kafka/src/main/java/com/example/spring_kafka/settings/KafkaSettings.java
@@ -0,0 +1,41 @@
+package com.example.spring_kafka.settings;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaSettings {
+
+    public Map<String, Object> consumerProperties() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "carlos-group-consumer");
+        // props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        // props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
+        // props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "150000");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG  , StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG  , StringDeserializer.class);
+        return props;
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerProperties());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> listenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> listenerContainerFactory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        listenerContainerFactory.setConsumerFactory(consumerFactory());
+        return listenerContainerFactory;
+    }
+}
